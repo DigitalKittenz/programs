5802511b4057c578f9e7cbb0b52fa6532b54bdb7
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2022-12-28 16:23:16 +0000

    deleted stuff

--- refs/ifelse in  one line.py
+++
@@ -1,3 +0,0 @@
-name, age, language = "8", 77, 99.5
-
-print(language)
--- refs/try-except.py
+++
@@ -1,7 +0,0 @@
-while True:
-    try:
-        data = int(input("Enter a Number: "))
-        print("You entered: ", data)
-        break;
-    except ValueError:
-        print("Invalid input")
